4ac15e59b365135d241c0f01a34c613c377a094a
Anže škulj <skuljanze95@gmail.com>
2022-01-22 18:53:55 +0100

    Add express, morgan

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello world");
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.json({ hello: 'world' });
+});
+
+const port = Number(process.env.PORT ?? 8080);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
