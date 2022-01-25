2fc86be54eccc42e1ec0618bbd7cb920d52aae41
Anže škulj <skuljanze95@gmail.com>
2022-01-25 20:32:16 +0100

    Remove vuex

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
+import { checkJwt } from '../middlewares/checkJwt';
 import auth from './auth';
 import project from './project';
 
 const routes = Router();
 
 routes.use('/api/auth', auth);
-routes.use('/api/project', project);
+routes.use('/api/project', [checkJwt], project);
 
 export default routes;
--- server/src/routes/project.ts
+++ server/src/routes/project.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
 import ProjectController from '../controllers/ProjectController';
-import { checkJwt } from '../middlewares/checkJwt';
 
 const router = Router();
 
-router.post('/createProject', [checkJwt], ProjectController.createProject);
+router.post('/createProject', ProjectController.createProject);
 
 export default router;
