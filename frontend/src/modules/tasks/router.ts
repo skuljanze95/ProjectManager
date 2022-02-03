14d56b0b8ec69e82224ea769502af96474e49fb5
Anže škulj <skuljanze95@gmail.com>
2022-02-03 13:52:23 +0100

    Remove comments

--- frontend/src/modules/tasks/router.ts
+++ frontend/src/modules/tasks/router.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { RouteRecordRaw } from 'vue-router';
 import Tasks from './views/Tasks.vue';
 
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -22,8 +22,6 @@ router.beforeEach((to, from, next) => {
   const authRequired = !publicPages.includes(to.path);
   const loggedIn = localStorage.getItem('user');
 
-  //trying to access a restricted page + not logged in
-  // redirect to login page
   if (authRequired && !loggedIn) {
     next('/login');
   } else {
